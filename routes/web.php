bfdf37bde494e9e78908214a965af8d1c4073ddc
Daniel Olivetti <50579682+quileab@users.noreply.github.com>
2025-11-19 18:55:47 -0300

    another try to fix shared hosting symlink

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Livewire\Volt\Volt;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 use App\Http\Controllers\ClearCacheController;
 
 //Volt::route('/', 'users.index');
@@ -73,19 +75,17 @@ Route::get('/products/type/{type}', function ($type) {
 Route::get('/products-web', \App\Livewire\ProductWebList::class)->name('products.web');
 
 
+Route::get('/storage/{file}', function ($file) {
+    $path = storage_path('app/public/' . $file);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    $fileContents = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($fileContents, 200);
+    $response->header("Content-Type", $type);
+    return $response;
+})->where('file', '.*')->name('storage.file');
+
 Route::get('/product/{product}', \App\Livewire\ProductDetail::class)->name('product.detail');
 Route::get('/post/{post}', \App\Livewire\PostDetail::class)->name('post.detail');
-
-Route::get('/create-storage-link', function () {
-    $targetFolder = storage_path('app/public');
-    $linkFolder = public_path('storage');
-    if (file_exists($linkFolder)) {
-        return 'The "public/storage" directory already exists.';
-    }
-    try {
-        symlink($targetFolder, $linkFolder);
-        return 'The [public/storage] directory has been linked.';
-    } catch (\Exception $e) {
-        return 'Error creating symlink: ' . $e->getMessage();
-    }
-});
